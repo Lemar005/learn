e1d0a018f762aa2db30187dcfb11eec99e99886b
Lemar005 <Lemar005@mail.ru>
2021-02-16 22:22:50 +0200

    rrrr

---
+++ Ecommerce-Stream/src/main/Application.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.ArrayList;
+
+import domain.Measurement;
+import domain.Price;
+import domain.Product;
+import domain.ProductFactory;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		
+//		Product p = ProductFactory.getProduct("iPhone");
+//		Product b = ProductFactory.getProduct("Samsung");
+//		Product s = ProductFactory.getProduct("");
+//		Product g = ProductFactory.getProduct(null);
+//		Product i = ProductFactory.getFakeProduct();
+		
+		Product v = ProductFactory.getProduct("Potato", 10, "USD", 5, "kg");
+	    System.out.println(v);
+
+//		ArrayList<Product> list = ProductFactory.getFakeProducts(10);
+//
+//		//System.out.println(p + "\n" + b +  "\n" + s + "\n" + g + "\n" + i);
+//    
+//        list.forEach(System.out::println);
+	}
+
+}
